0fb070958ee83c2e5d1da6c8d334a8931ec33bbe
Ferenc Szalai <szferi@pionlab.com>
2017-11-26 20:55:48 +0100

    add missing setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='navega-google-colab',
+      version='0.1',
+      description='Navega Utils working with colab.google',
+      author='Navegastrategies',
+      author_email='info@navegastrategies.com',
+      url='http://navegastrategies.com',
+      packages=['navegacolab', 
+               ],
+)
+
