f14413494da150ef0ee3f933c5a0535469a51235
Ferenc Szalai <szferi@pionlab.com>
2017-11-27 00:26:01 +0100

    .

--- navegacolab/__init__.py
+++ navegacolab/__init__.py
@@ -7,12 +7,12 @@ from .config import DEFAULT_CONFIG
 from .drive import GDrive
 
 
-def init_account(config=None):
+def init_account(config=None, user_email=None):
     if config is None:
         config = DEFAULT_CONFIG
     if os.environ.get('GOOGLE_APPLICATION_CREDENTIALS') is None:
         auth.authenticate_user()
-    drive = GDrive()
+    drive = GDrive(user_email=user_email)
     for name in ['service_account', 'ssh_private_key', 'ssh_public_key', 'ssh_config']:
         c = config.get('paths').get(name)
         drive.download(c.get('drive'), c.get('local'))
