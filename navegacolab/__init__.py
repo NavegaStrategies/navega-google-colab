669668aabceafc9bb7bcadb3f15b5570bc864456
Ferenc Szalai <szferi@pionlab.com>
2017-11-26 22:09:55 +0100

    .

--- navegacolab/__init__.py
+++ navegacolab/__init__.py
@@ -5,10 +5,8 @@ from pydrive.drive import GoogleDrive
 from .drive import GDrive
 
 
-def get_service_account(drive_path, local_path='service_account.json'):
+def init_account(drive_path, local_path='service_account.json'):
     if not os.path.exists(local_path):
         auth.authenticate_user()
         drive = GDrive()
-        drive_id, fname, folder_id = drive.path_to_id(drive_path)
-        service_account_file = drive.CreateFile({'id': drive_id})
-        service_account_file.GetContentFile(local_path)
+        drive.download(drive_path, local_path)
--- navegacolab/drive.py
+++ navegacolab/drive.py
@@ -43,6 +43,11 @@ class GDrive(object):
             q = "'{folder_id}' in parents and trashed=false".format(folder_id=fid)
         return self.drive.ListFile({'q': q}).GetList()
 
+    def download(self, drive_path, local_path):
+        drive_id, fname, folder_id = self.path_to_id(drive_path)
+        f = self.drive.CreateFile({'id': drive_id})
+        return f.GetContentFile(local_path)
+    
     def path_to_id(self, path):
         folder_name, file_name = os.path.dirname(path), os.path.basename(path)
         folder_id = self.folder_to_id(folder_name)
